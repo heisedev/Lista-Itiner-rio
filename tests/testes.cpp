a87dbb1c30a129bfe68f27da03fb521ed84aaa57
DESKTOP-0CDRRID\Administrador <suporte@univali.br>
2026-03-24 20:24:48 -0300

    Versionando projeto

--- tests/testes.cpp
+++ tests/testes.cpp
@@ -2,21 +2,4 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    ListaParadas rota;
-    rota.inserirInicio("Terminal");
-    rota.inserirFim("Shopping");
-    rota.inserirPosicao("Praça Central", 2);
 
-    rota.listarIda();
-    rota.listarVolta();
-
-    rota.buscar("Praça");
-    cout << "Total de paradas: " << rota.contar() << endl;
-
-    rota.removerInicio();
-    rota.removerFim();
-    rota.listarIda();
-
-    return 0;
-}
