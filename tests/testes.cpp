0efb0082a0ee052b04372aae8cec4f4b53f66482
heisedev <contatohldsn@gmail.com>
2026-03-30 22:23:49 -0300

    Ajuste final

--- tests/testes.cpp
+++ tests/testes.cpp
@@ -1,5 +1,19 @@
-#include "include/ListaParadas.h"
-#include <iostream>
-using namespace std;
+int main() {
+    ListaParadas rota;
+    rota.inserirInicio("Terminal");
+    rota.inserirFim("Shopping");
+    rota.inserirPosicao("Praça Central", 2);
 
+    rota.listarIda();
+    rota.listarVolta();
 
+    rota.buscar("Praça");
+    cout << "Total de paradas: " << rota.contar() << endl;
+
+    rota.removerInicio();
+    rota.removerFim();
+    rota.listarIda();
+
+    
+    return 0;
+}
